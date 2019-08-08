cab4418009f71bd7574f0580bed7b6639ff81e8b
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-08 16:48:47 +0300

    Added console project

---
+++ Cs.Itself.Structures/Cs.Itself.Structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs.Itself.Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
